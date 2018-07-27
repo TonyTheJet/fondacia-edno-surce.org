b65ad497b6ff9faa5e2fb84b53c94e6361386028
TonyTheJet <tony@tony-anderson.info>
2018-07-26 22:33:16 -0600

    implements all the pages and content

--- functions.php
+++ functions.php
@@ -1,1 +1,20 @@
 <?php
+
+// theme support
+add_theme_support( 'post-thumbnails' );
+
+/**
+ * @return WP_Post[]
+ */
+	function fes_home_child_posts(): array {
+		return get_posts(
+			[
+				'order' => 'ASC',
+				'orderby' => 'menu_order',
+				'post_parent' => get_option('page_on_front'),
+				'post_status' => 'publish',
+				'post_type' => 'page',
+				'posts_per_page' => 999
+			]
+		);
+	}
--- page.php
+++ page.php
@@ -1,8 +1,33 @@
 <?php
 	get_header();
+	$posts = fes_home_child_posts();
 ?>
     <div class="container" id="main-content">
-        Content coming soon!
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>Фундация Едно Сърце</h1>
+            </div>
+        </div>
+        <?php foreach ($posts as $post): ?>
+            <div class="row home-section" id="post-<?php $post->ID; ?>">
+                <div class="col-sm-12">
+                    <h2><?php echo $post->post_title; ?></h2>
+                    <div class="post-content">
+                        <?php if (has_post_thumbnail($post)): ?>
+                            <div class="float-right col-sm-12 col-md-7 col-lg-6">
+	                            <?php
+                                    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
+                                ?>
+                                <img src="<?php echo $image[0]; ?>" alt="featured image" class="img-fluid post-img" />
+                            </div>
+                        <?php endif; ?>
+                        <div class="col-sm-12 col-md-5 col-lg-6">
+	                        <?php echo apply_filters('the_content', $post->post_content); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
 <?php
 	get_footer();
